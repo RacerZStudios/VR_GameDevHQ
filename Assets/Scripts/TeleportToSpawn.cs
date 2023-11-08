29e359a9c9ecb37f395eb00aee45474b38e5a8d2
Zac <zac.bogner@gmail.com>
2023-11-08 11:29:14 -0700

    VR event 5 package and prefab export spawn polish

--- Assets/Scripts/TeleportToSpawn.cs
+++ Assets/Scripts/TeleportToSpawn.cs
@@ -10,13 +10,30 @@ public class TeleportToSpawn : MonoBehaviour
     [SerializeField]
     public Transform spawn;
 
+    [SerializeField]
+    public BoxCollider cabCollider;
+
+    [SerializeField]
+    private bool canInteract; 
+
     private void Update()
     {
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
-            playerPos.gameObject.GetComponent<CharacterController>().enabled = false; 
-            playerPos.position = spawn.position;
-            playerPos.gameObject.GetComponent<CharacterController>().enabled = true; 
+            canInteract = true;
+            if(canInteract == true)
+            {
+                playerPos.gameObject.GetComponentInParent<CharacterController>().enabled = false;
+                playerPos.position = spawn.position;
+                playerPos.gameObject.GetComponentInParent<CharacterController>().enabled = true;
+            }
         }
+    }
+
+    private IEnumerator EnableCollision()
+    {
+        cabCollider.gameObject.GetComponentInParent<BoxCollider>().enabled = false;
+        yield return new WaitForSeconds(4);
+        cabCollider.gameObject.GetComponentInParent<BoxCollider>().enabled = true;
     }
 }
